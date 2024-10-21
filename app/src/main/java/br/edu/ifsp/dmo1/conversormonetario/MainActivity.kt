ffdb59b37f25de1d2e6630562a9263b15bc6f99a
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-10-21 14:22:18 -0300

    Adição da funcionalidade de conversão para real, fazendo os ajustes necessários na MainActivity.

--- app/src/main/java/br/edu/ifsp/dmo1/conversormonetario/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/conversormonetario/MainActivity.kt
@@ -13,7 +13,8 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     private val DOLLAR_VALUE = 5.50
 
     private lateinit var inputEditText: EditText
-    private lateinit var calculateButton: Button
+    private lateinit var toDollarButton: Button
+    private lateinit var toRealButton: Button
     private lateinit var outputTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,29 +26,42 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     }
 
     override fun onClick(v: View) {
-        if(v == calculateButton){
-            makeConvertion()
+        if(v == toDollarButton){
+            dollarConversion()
+        }else if(v == toRealButton){
+            realConversion()
         }
     }
 
     private fun findById() {
         inputEditText = findViewById(R.id.edittext_value)
-        calculateButton = findViewById(R.id.button_calculate)
+        toDollarButton = findViewById(R.id.button_convert_to_dollar)
+        toRealButton = findViewById(R.id.button_convert_to_real)
         outputTextView = findViewById(R.id.textview_output)
     }
 
     private fun configClickListener() {
-        calculateButton.setOnClickListener(this)
+        toDollarButton.setOnClickListener(this)
+        toRealButton.setOnClickListener(this)
     }
 
-    private fun makeConvertion() {
-        var value = try{
+    private fun getValue(): Double{
+        return try{
             inputEditText.text.toString().toDouble()
         }catch (e: NumberFormatException){
             0.0
         }
-        value = value / DOLLAR_VALUE
+    }
 
+    private fun dollarConversion() {
+        var value = getValue()
+        value = value / DOLLAR_VALUE
         outputTextView.text = "U$ $value"
+    }
+
+    private fun realConversion(){
+        var value = getValue()
+        value = value * DOLLAR_VALUE
+        outputTextView.text = "R$ $value"
     }
 }
